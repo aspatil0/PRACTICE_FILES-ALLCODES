ca59edca8a1d409c25a941fb295168629f6dd56b
ADITYARAJ PATIL <119296947+aspatil0@users.noreply.github.com>
2025-08-27 00:51:35 +0530

    Create prime.js

---
+++ prime.js
@@ -0,0 +1,16 @@
+function Primes(num) {
+  num = Number(num); 
+
+  if (num < 2) return 'false';
+
+  for (let i = 2; i <= Math.sqrt(num); i++) {
+    if (num % i === 0) {
+      return 'false';
+    }
+  }
+
+  return 'true';
+}
+
+// keep this function call here
+console.log(Primes(readline()));
